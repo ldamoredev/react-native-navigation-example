cc8b00856c54cccf8ea1b14e29411d291b78cd55
Lautaro Damore <lauti.damore@gmail.com>
2024-09-25 21:28:25 -0300

    navigate

--- src/ui/screens/HomeScreen.tsx
+++ src/ui/screens/HomeScreen.tsx
@@ -2,14 +2,16 @@ import * as React from 'react'
 import { Button, Text, View } from 'react-native'
 import { ScreenDefinition } from '../navigation/screensDefinitions/ScreenDefinition'
 import { ScreenVisibilities } from '../navigation/screensDefinitions/ScreenVisibilities'
+import { useAppContext } from '../context/AppContext'
 
 export const HomeScreen: React.FC = () => {
+    const { navigator } = useAppContext()
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Home screen</Text>
             <Button
                 title="Go to Details"
-                onPress={() => {}}
+                onPress={() => navigator.navigate('Details')}
             />
         </View>
     );
